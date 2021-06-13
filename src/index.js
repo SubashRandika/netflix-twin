99dc97b9ae37c067286f544aa471ec2bc917bfbb
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-06-13 23:43:20 +0530

    Created SignIn page form and connected with firebase. Styled with styled components.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { firebase } from './lib/firebase.prod';
 import { FirebaseContext } from './context/firebase';
 
 render(
-	<FirebaseContext.Provider values={{ firebase }}>
+	<FirebaseContext.Provider value={{ firebase }}>
 		<GlobalStyles />
 		<App />
 	</FirebaseContext.Provider>,
